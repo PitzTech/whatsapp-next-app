9b4f370d50a34ff52b7b8fbea56d6b7286d75ded
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-06-24 20:13:23 -0300

    Update Login Screen

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,26 +1,31 @@
 import Head from "next/head"
 import Image from "next/image"
 
-import { LoginContainer, Logo } from "../styles/pages/Login"
+import {
+	LoginScreenContainer,
+	LoginContainer,
+	Logo,
+	GoogleButton
+} from "../styles/pages/Login"
 
 import logoImg from "../assets/logo.svg"
 import googleIconImg from "../assets/google-icon.svg"
 
 function Login(): JSX.Element {
 	return (
-		<div>
+		<LoginScreenContainer>
 			<Head>
 				<title>Login</title>
 			</Head>
 
 			<LoginContainer>
-				<Logo src={logoImg} />
-				<button className="google-login-button">
+				<Logo width={200} height={200} src={logoImg} />
+				<GoogleButton className="google-login-button">
 					<Image src={googleIconImg} alt="Logo do Google" />
 					<span className="google-text">Sign in with Google</span>
-				</button>
+				</GoogleButton>
 			</LoginContainer>
-		</div>
+		</LoginScreenContainer>
 	)
 }
 
--- styles/pages/Login.ts
+++ styles/pages/Login.ts
@@ -1,34 +1,55 @@
 import styled from "styled-components"
 import Image from "next/image"
-import { Button } from "@material-ui/core"
+
+export const LoginScreenContainer = styled.div`
+	height: 100vh;
+
+	display: grid;
+	place-items: center;
+
+	background-color: var(--secondary-background);
+`
 
 export const LoginContainer = styled.section`
-	.google-login-button {
-		margin-top: 2rem;
-		height: 5rem;
-		padding: 2rem;
-		border-radius: 8px;
-		font-weight: 500;
-		background: #ea4335;
-		color: #fff;
+	position: relative;
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
+	display: flex;
+	flex-direction: column;
 
-		cursor: pointer;
-		border: 0;
+	padding: 8rem;
+	border-radius: 2rem;
 
-		transition: filter 0.2s;
-
-		.google-text {
-			margin-left: 1rem;
-		}
-
-		&:hover {
-			filter: brightness(0.9);
-		}
-	}
+	background-color: var(--primary-background);
+	box-shadow: 0px 3px 14px -3px rgba(0, 0, 0, 0.5);
 `
 
 export const Logo = styled(Image)``
+
+export const GoogleButton = styled.button`
+	margin-top: 4rem;
+	height: 5rem;
+	padding: 0 5rem;
+	border: 0;
+	border-radius: 8px;
+
+	font-weight: 500;
+	background: #ea4335;
+	box-shadow: 0px 5px 10px -1px rgba(0, 0, 0, 0.5);
+	color: #fff;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	cursor: pointer;
+
+	transition: filter 0.2s;
+
+	.google-text {
+		margin-left: 1rem;
+	}
+
+	&:hover {
+		filter: brightness(0.9);
+	}
+`
